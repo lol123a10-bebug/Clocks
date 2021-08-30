89405f464bbd55c3944ae90a1473c4dee97e1a35
lol123a10-bebug <ayubkhon.gulomov@gmail.com>
2021-08-31 01:38:40 +0500

    Feature: Background-changer

--- src/components/Hall.js
+++ src/components/Hall.js
@@ -1,5 +1,19 @@
+import { useMemo } from "react";
+import { useContext } from "react";
+import Context from "../store/context";
+
 const Hall = () => {
-  return <div></div>;
+  const ctx = useContext(Context);
+
+  const background = useMemo(() => {
+    if (ctx.second % 4 === 0) {
+      return `rgba(${Math.random() * 100}, ${Math.random() * 100}, ${
+        Math.random() * 100
+      }, 1)`;
+    }
+  }, [ctx.second]);
+
+  return <div style={{ background }}>{ctx.second}</div>;
 };
 
 export default Hall;
